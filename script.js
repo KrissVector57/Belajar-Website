ac4bc4096b3ce0387f2e9134173a55918dab17a0
Kriss <krissvector579@gmail.com>
2026-07-16 13:36:19 +0700

    add dark mode

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function hello() {
-    alert("Hello, GitHub! 🚀");
-}
+// Smooth Scroll
+document.querySelectorAll("nav a").forEach(link => {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        target.scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+
+// Dark Mode
+const button = document.getElementById("darkModeBtn");
+
+button.onclick = function () {
+    document.body.classList.toggle("dark-mode");
+};
