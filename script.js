1a73d1f7cd2a8ba449003ee49f56438acc082f16
Kriss <krissvector579@gmail.com>
2026-07-16 14:20:46 +0700

    actv nav

--- script.js
+++ script.js
@@ -74,3 +74,35 @@ function type() {
 }
 
 type();
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 150;
+
+        if (window.scrollY >= sectionTop) {
+
+            current = section.getAttribute("id");
+
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+
+            link.classList.add("active");
+
+        }
+
+    });
+
+});
